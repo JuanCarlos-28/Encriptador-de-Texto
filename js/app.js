77be582a5a98577e4d85effb2ee129f777008ef8
Juan Carlos Rivas <juan_carlosr223@hotmail.com>
2023-04-12 01:51:06 -0600

    Encriptación de texto

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+ 
+
+//* Obtener los elementos
+// const texto = document.querySelector('.caja');
+// const btnEncriptar = document.querySelector('.btn-encriptar')
+// const btnDesencriptar = document.querySelector('.btn-desencriptar')
+
+
+
+function encriptar() {
+  const texto = document.querySelector('.caja').value.toLowerCase(); // gael
+  let aside = document.querySelector('.aside');
+  let textoEncriptado = texto.replace(/e/g, "enter");
+  textoEncriptado = textoEncriptado.replace(/i/g, "imes");
+  textoEncriptado = textoEncriptado.replace(/a/g, "ai"); //gaiel
+  textoEncriptado = textoEncriptado.replace(/o/g, "ober");
+  textoEncriptado = textoEncriptado.replace(/u/g, "ufat");
+
+  console.log(textoEncriptado);
+  aside.innerHTML = `
+  <textarea rows="15" class="mensaje-encriptado" readonly>${textoEncriptado}</textarea>
+  <button class="btn-copiar" onclick="copy()">Copiar</button>`
+}
+
+function desencriptar() {
+  const texto = document.querySelector('.caja').value.toLowerCase(); // gael
+  let aside = document.querySelector('.aside');
+  let textoEncriptado = texto.replace(/enter/g, "e");
+  textoEncriptado = textoEncriptado.replace(/imes/g, "i");
+  textoEncriptado = textoEncriptado.replace(/ai/g, "a"); //gaiel
+  textoEncriptado = textoEncriptado.replace(/ober/g, "o");
+  textoEncriptado = textoEncriptado.replace(/ufat/g, "u");
+
+  console.log(textoEncriptado);
+  aside.innerHTML = `
+    <textarea rows="15" class="mensaje-encriptado" readonly>${textoEncriptado}</textarea>
+    <button class="btn-copiar" onclick="copy()">Copiar</button>`
+}
+
+function copy() {
+  let copyText = document.querySelector(".mensaje-encriptado");
+  copyText.select();
+  document.execCommand("copy");
+}
+
